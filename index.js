32a4c5eca30996df74f6e8f56a5ed5a14b0e40e9
shashwat2710 <40847974+shashwat2710@users.noreply.github.com>
2022-10-07 09:45:49 +0530

    basic express setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express =  require('express');
+
+const app =  express();
+
+app.get('/', (req,res)=>{
+    res.send('<h2>Hi There </h2>')
+})
+
+const PORT =  process.env.port || 3000
+
+app.listen(PORT, ()=> console.log(`Server Running on Port ${PORT}`));
