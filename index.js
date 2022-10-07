ef3fb43f3f0a48bae128ea7fc12641410af4be21
shashwat2710 <40847974+shashwat2710@users.noreply.github.com>
2022-10-07 17:20:26 +0530

    mongo db intialization and connect completed

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express =  require('express');
+const mongoose = require('mongoose');
+
+const {MONGO_USER, MONGO_PASS, MONGO_IP, MONGO_PORT}  = require('./config/config');
+
 
 const app =  express();
 
+const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = ()=>{
+    mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log(`Successfully connected to database`)
+    }).catch(e=> {console.log(e);console.log('Trying to connect to database...')
+    
+    setTimeout(connectWithRetry,5000)});
+
+}
+
+connectWithRetry();
+
 app.get('/', (req,res)=>{
-    res.send('<h2>Hi There!</h2>')
+    res.send('<h2>Hi There!!</h2>')
 })
 
 const PORT =  process.env.port || 3000
